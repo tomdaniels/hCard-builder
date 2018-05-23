4c4b76dac785b25a032fe1f05ace66871680d369
tomdaniels <thomsondaniels@hotmail.com>
2018-05-23 13:46:52 +1000

    inital commit, get dev server running, webpack and dev dependencies

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Landing from './components/landing.js';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<Landing />, document.getElementById('app'));
---
+++ src/components/landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Landing = () => (
+    <div className="styles">
+        Replace me with an app!
+    </div>
+);
+
+export default Landing;
